74632424ac0fdb6c279c390cff292af7383c3bde
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-07 11:05:51 +0100

    first commit

---
+++ Näsan List/array övning 2/Program.cs
@@ -0,0 +1,22 @@
+﻿string typedInThing = "";
+List<string> cities = new List<string>();
+
+
+
+Console.WriteLine("Type whatever and it'll get put into the list, type exit to leave.");
+while (typedInThing != "exit") 
+{
+    typedInThing = Console.ReadLine().ToLower();
+    if (typedInThing != "exit")
+    {
+    cities.Add(typedInThing);
+    Console.WriteLine($"Added {typedInThing}");
+    }
+}
+
+Console.WriteLine("You've added:");
+for (int i = 0; i < cities.Count; i++)
+{
+    Console.WriteLine($"{cities[i]}");
+    Console.ReadLine();
+}
